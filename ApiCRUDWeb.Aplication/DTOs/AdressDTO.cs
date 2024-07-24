d5da58f0999c568cbb8467adbd679878778a850a
Eric Silva <ericgoleiro2004@gmail.com>
2024-07-23 22:37:06 -0300

    Added Feature Update Adress

--- ApiCRUDWeb.Aplication/DTOs/AdressDTO.cs
+++ ApiCRUDWeb.Aplication/DTOs/AdressDTO.cs
@@ -1,13 +1,28 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace ApiCRUDWeb.Aplication.DTOs
 {
 	public class AdressDTO
+		
 	{
+		[Required]
+		[StringLength(30, MinimumLength = 4, ErrorMessage = "O País deve conter entre 4 a 30 caracteres")]
 		public string Country { get; set; }
+		[Required]
+		[StringLength(50, MinimumLength = 4, ErrorMessage = "O Estado deve conter entre 4 a 50 caracteres")]
 		public string State { get; set; }
+		[Required]
+		[StringLength(50, MinimumLength = 4, ErrorMessage = "A cidade deve conter entre 4 a 50 caracteres")]
 		public string City { get; set; }
+		[Required]
+		[StringLength(50, MinimumLength = 4, ErrorMessage = "O bairro deve conter entre 4 a 50 caracteres")]
 		public string Neighborhood { get;set; }
+		[Required]
+		[StringLength(150, MinimumLength = 4, ErrorMessage = "O logradouro deve conter entre 4 a 150 caracteres")]
 		public string PublicPlace { get; set; }
+		[Required]
+		[StringLength(20, MinimumLength = 4, ErrorMessage = "O Complemento deve conter entre 4 a 20 caracteres")]
 		public string Complement { get; set; }
 	}
 }
